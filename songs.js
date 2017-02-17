c94faeee2dad7ffa5133d8c0c20c723060e1c5b1
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-16 20:59:34 -0600

    finish the music history 2

--- songs.js
+++ songs.js
@@ -1,56 +1,61 @@
-// /*Albums array */
-// var drakeAlbum = [
-// "take care" , 
-// "Nothing Was The Same",
+/*Albums array */
+var drakeAlbum = [
+"take care" , 
+"Nothing Was The Same",
 
-// ]
+]
 
-// var drakeAlbum1 = {
-//      album: "take care",
-//     Artist:  "drake",
-//     "Song name":  "Over My Dead Body",
-// }
+var drakeAlbum1 = {
+     album: "take care",
+    Artist:  "drake",
+    "Song name":  "Over My Dead Body",
+}
 
-// var drakeAlbum2 = {
-//      album: "Over My Dead Body",
-//     Artist:  "drake",
-//     "Song name":  "Over My Dead Body",
-// }
+var drakeAlbum2 = {
+     album: "Over My Dead Body",
+    Artist:  "drake",
+    "Song name":  "Over My Dead Body",
+}
 
 
-// var drakeTakeCareSongs = [
-// "Over My Dead Body" ,
-// "Shot For Me" ,
-// "Headlines"
-// ]
+var drakeTakeCareSongs = [
+"Over My Dead Body" ,
+"Shot For Me" ,
+"Headlines"
+]
 
 var songs = [
 
 ];
 
+//Each student must add one song to the beginning   
+songs[songs.length] = "Cheap Thrills by Sia on the album This is Acting";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+//Each student must add one song to the end of the array.
+songs[songs.length] = "Deja Vu by Mike Posner on the album 31 Minutes to Takeoff";
 
-var i
-for( i=0 ; i<songs.length ; i++) {
-	var Songs = songs[i].innerHTML;
+var songsArray = ["mySong1","mySong2","mySong3","mySong4"];
+
+
+//Loop over the array and remove any words or characters that obviously don't belong.
+//Students must find and replace the > character in each item with a - character.
+
+for( var i=0  ; i<songs.length  ; i++  ) {
 	songs[i] = songs[i].replace(" >" , " - ");
 	songs[i] = songs[i].replace("*" , '');
 	songs[i] = songs[i].replace("@" , '');
 	songs[i] = songs[i].replace(" & " ,'');
 	songs[i] = songs[i].replace("!" , '');
 	songs[i] = songs[i].replace("(" , '');
-	// document.getElementById("mySong").innerHtml += '<li>' + songs[i].innerHtml + '</li>' ;
-	document.getElementById("mySong1").innerHTML += '<li>' + songs[i] + '</li>' ;
-	document.getElementById("mySong2").innerHTML += '<li>' + songs[i] + '</li>' ;
-	document.getElementById("mySong3").innerHTML += '<li>' + songs[i] + '</li>' ;
-	document.getElementById("mySong4").innerHTML += '<li>' + songs[i] + '</li>' ;
+    
+	document.getElementById("mySong" + i ).innerHTML = '<li>' + songs[i] + '</li>' ;
+	
 }
 
-	// console.log (songs);
 	
 
 
