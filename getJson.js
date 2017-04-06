35a6e7950fc374a3405d9e8f891f2e734d74e176
Sanyyouisf <sanymamdoh@yahoo.com>
2017-04-06 00:44:15 -0500

    start part 2

--- getJson.js
+++ getJson.js
@@ -1,33 +1,40 @@
  
 //Read from local JSON file with an XHR.
 var xhr = new XMLHttpRequest();
+var xhr2 = new XMLHttpRequest();
+var songContainer = document.getElementById("songContainer");
+var song2Container = document.getElementById("song2Container");
+var button = document.getElementById("button");
 
 
 //Loop over results and inject into Music History list view.
 function makeDOM(xhrData){
 	var mySong={}
 	for( var i=0; i< xhrData.songs.length ;i++){
-		mySong.Artist = `<div id= "container-${i}"> <p class="seb">  Artist : ${xhrData.songs[i].Artist}</p>`;
-		mySong.album = ` <p class="seb">  album name : ${xhrData.songs[i].album}</p>`;
-		mySong.SongName = ` <p class="seb">  Song  : ${xhrData.songs[i].SongName}</p>`;
+		mySong.Artist = `<div id= "container-${i}" class = "container"> <p>  Artist : ${xhrData.songs[i].Artist}</p>`;
+		mySong.album = ` <p>  album name : ${xhrData.songs[i].album}</p>`;
+		mySong.SongName = ` <p>  Song  : ${xhrData.songs[i].SongName}</p>`;
 		deletbtn = `<input type="button" class="btn" id ="button-${i}" value ="delete"> </div>`;
 		document.getElementById("mySong" + i ).innerHTML = '<li>' + mySong.Artist + '</li>' 
 		+ '<li>' + mySong.album + '</li>'
 		+'<li>' + mySong.SongName + '</li>'
 		+ deletbtn ;	
 	}
+	//Add a button at the bottom of your music list and label it More >
+	var button = ' <input type="button" id ="button" class="morebtn" value="More >"> ' ;
+	songContainer.innerHTML += button
 }
 
 
 var data
- function excuteWhenLoad (){
-    data = JSON.parse(this.responseText);
- 	makeDOM(data);
- }
+function excuteWhenLoad (){
+   data = JSON.parse(this.responseText);
+   makeDOM(data);
+}
 
- xhr.addEventListener("load",excuteWhenLoad);
- xhr.open("GET","songs.json");
- xhr.send();
+xhr.addEventListener("load",excuteWhenLoad);
+xhr.open("GET","songs.json");
+xhr.send();
 
 //Add delete button to each row and, when it is clicked, delete the entire row in the DOM.
 function deletSong(event){
@@ -41,3 +48,41 @@ function deletSong(event){
 
 // event listener for the delete button
 document.body.addEventListener("click" ,deletSong);
+
+
+//the code for part 2 
+function makeDOMSongs2(xhrData2){
+	var mySong2={}
+	for( var i=0; i< xhrData2.songs2.length ;i++){
+		mySong2.Artist = `<div id= "container-${i}" class = "container"> <p>  Artist : ${xhrData2.songs2[i].Artist}</p>`;
+		mySong2.album = ` <p>  album name : ${xhrData2.songs2[i].album}</p>`;
+		mySong2.SongName = ` <p>  Song  : ${xhrData2.songs2[i].SongName}</p>`;
+		deletbtn = `<input type="button" class="btn" id ="button-${i}" value ="delete"> </div>`;
+		document.getElementById("mySong" + i ).innerHTML = '<li>' + mySong2.Artist + '</li>' 
+		+ '<li>' + mySong2.album + '</li>'
+		+'<li>' + mySong2.SongName + '</li>'
+		+ deletbtn ;
+	}
+}
+
+function loadSongs2(){
+    data2 = JSON.parse(this.responseText);
+ 	makeDOMSongs2(data2);
+ 	// console.log("data2",data2);
+ }
+
+
+document.body.addEventListener("click",function(){
+	console.log ("more songs");
+	xhr2.addEventListener("load",loadSongs2);
+	xhr2.open("GET","songs2.json");
+	xhr2.send();
+});
+
+
+
+
+
+
+
+
