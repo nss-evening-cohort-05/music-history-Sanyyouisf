93255724eb3b8d5c6d43b2237158f5d902a9a615
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-19 23:57:00 -0500

    addind files and directories

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
     //make DOM for the first jason
     function makeDOM(data) {
-        var mySong = {}
+        var mySong = {};
         for (var i = 0; i < data.songs.length; i++) {
             mySong.Artist = `<div id= "container-${i}" class = "container"> <p>the Artist is : ${data.songs[i].Artist}</p>`;
             mySong.album = ` <p>album : ${data.songs[i].album}</p>`;
@@ -35,15 +35,14 @@ $(document).ready(function() {
 
     //make DOM for the second jason
     function makeDOM2(data2) {
-        var mySong = {}
+        var mySong = {};
         for (var i = 0; i < data2.songs2.length; i++) {
             mySong.Artist = `<div id= "container-${i}" class = "container"> <p>the Artist is : ${data2.songs2[i].Artist}</p>`;
             mySong.album = ` <p>album : ${data2.songs2[i].album}</p>`;
             mySong.SongName = ` <p> Song : ${data2.songs2[i].SongName}</p>`;
             $("#2mySong" + i).html('<li>' + mySong.Artist + '</li>' + '<li>' + mySong.album + '</li>' + '<li>' + mySong.SongName + '</li>' + deletbtn);
         }
-        $('#song2Container').append(moreBtn);
-    };
+    }
 
     //event listener for the more button
     $(document).on("click", "#button", function() {
@@ -51,9 +50,9 @@ $(document).ready(function() {
         $.ajax({
             url: "songs2.JSON"
         }).done(function(songs2) {
-            makeDOM2(songs2)
+            makeDOM2(songs2);
         }).fail(function(error) {
-            console.log("the following error occurred", error)
+            console.log("the following error occurred", error);
         });
     });
 
